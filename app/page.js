9a9615a4ff79a72aa4f32a3feefe75e651ea92cb
shourovahmd <moh.abuhanifa@gmail.com>
2023-06-29 23:45:49 +0600

    hero section

--- app/page.js
+++ app/page.js
@@ -1,9 +1,11 @@
+import Hero from "@/components/Hero";
 import Nav from "@/components/Nav";
 
 export default function Home() {
   return (
     <main className="w-[1400px] mx-auto">
       <Nav />
+      <Hero/>
     </main>
   );
 }
